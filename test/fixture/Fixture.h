9cc31282fc362f5a53e5274a804233f2fe69b065
GiSeong Ji <jiggyjiggy0323@gmail.com>
2024-11-17 22:20:28 +0900

    refactor: fixture 구현부 분리

---
+++ test/fixture/Fixture.h
@@ -0,0 +1,13 @@
+#ifndef FIXTURE_H
+#define FIXTURE_H
+
+#include <string>
+
+#include "cctv/Client.h"
+
+namespace fixture
+{
+	extern cctv::host_t cctv1;
+}
+
+#endif /* FIXTURE_H */
--- test/fixture/fixture.cpp
+++ test/fixture/fixture.cpp
@@ -1,9 +1,9 @@
-#include "cctv/Client.h"
+#include "Fixture.h"
 
 namespace fixture
 {
-	cctv::host_t cctv1 { 
-		.ip = "192.168.50.14",
-		.port = 12345
-	};
+    cctv::host_t cctv1 = { 
+        .ip = "192.168.50.14",
+        .port = 12345
+    };
 }
