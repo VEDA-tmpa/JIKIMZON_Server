b3a2ba5ae537e880b21af4226453b169bac95aec
GiSeong Ji <jiggyjiggy0323@gmail.com>
2024-12-05 15:50:37 +0900

    test : 사이즈 테스트

--- test/cipher_net/step2-2/client.cpp
+++ test/cipher_net/step2-2/client.cpp
@@ -10,8 +10,8 @@
 
 #include "common/cipher/ChaCha20.h"
 
-#define SERVER_IP "192.168.10.99" // 서버 IP
-// #define SERVER_IP "127.0.0.1" // 서버 IP
+// #define SERVER_IP "192.168.10.99" // 서버 IP
+#define SERVER_IP "127.0.0.1" // 서버 IP
 #define SERVER_PORT 12345     // 서버 포트 (서버와 동일하게 설정)
 
 // 클라이언트 메인 함수
@@ -98,15 +98,29 @@ int main()
     }
     std::cout << std::dec << std::endl;
 
-	// std::vector<uint8_t> nonce(12, 0x00);
-	// std::string timestamp = "2024112691234569789";
+	std::vector<uint8_t> nonce2(12, 0x00);
+	std::string timestamp = "20241126_123456.789";
+	std::cout << "before nonce2 size: " << nonce2.size() << std::endl;
 
-	// // timestamp의 마지막 12문자를 uint8_t로 변환 후 nonce에 복사
-	// std::transform(timestamp.end() - 12, timestamp.end(), nonce.begin(),
-	// 			[](char c) { return static_cast<uint8_t>(c); });
+	// timestamp의 마지막 12문자를 uint8_t로 변환 후 nonce2에 복사
+	std::transform(timestamp.end() - 12, timestamp.end(), nonce2.begin(),
+				[](char c) { return static_cast<uint8_t>(c); });
+	std::cout << "after nonce2 size: " << nonce2.size() << std::endl;
+	// nonce 값 출력
+    std::cout << "Nonce2: ";
+    for (uint8_t c : nonce2) {
+        std::cout << static_cast<char>(c);
+    }
+    std::cout << std::endl;
+	std::cout << "Nonce2 (as bytes): ";
+    for (uint8_t c : nonce2) {
+        std::cout << "0x" << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(c) << " ";
+    }
+    std::cout << std::dec << std::endl;
+
 
 	std::vector<uint8_t> nonce = { 0x36, 0x5f, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x2e, 0x37, 0x38, 0x39 };
-
+	std::cout << "nonce size: " << nonce.size() << std::endl;
 
 	// nonce 값 출력
     std::cout << "Nonce: ";
