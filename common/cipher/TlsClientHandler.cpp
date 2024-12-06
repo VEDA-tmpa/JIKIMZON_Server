832310556cb4ea910a8060e2c7ae9800bfe49792
GiSeong Ji <jiggyjiggy0323@gmail.com>
2024-12-06 19:58:40 +0900

    chore: 디버그 메시지 추가

--- common/cipher/TlsClientHandler.cpp
+++ common/cipher/TlsClientHandler.cpp
@@ -13,14 +13,20 @@ namespace cipher
 
 	void TlsClientHandler::Init()
 	{
+		std::cout << "TlsClientHandler::Init() start" << std::endl;
+
 		SSL_library_init();
 		SSL_load_error_strings();
 		OpenSSL_add_ssl_algorithms();
 		createSSLContext();
+
+		std::cout << "TlsClientHandler::Init() end" << std::endl;
 	}
 
 	void TlsClientHandler::createSSLContext()
 	{
+		std::cout << "TlsClientHandler::createSSLContext() start" << std::endl;
+
 		const SSL_METHOD* method = TLS_client_method(); // 클라이언트용
 		mCTX = SSL_CTX_new(method);
 		if (!mCTX)
@@ -29,12 +35,19 @@ namespace cipher
 			ERR_print_errors_fp(stderr);
 			exit(EXIT_FAILURE);
 		}
+
+		std::cout << "TlsClientHandler::createSSLContext() end" << std::endl;
 	}
 
 	void TlsClientHandler::PerformTLSHandshake(int clientFd)
 	{
+		std::cout << "TlsClientHandler::PerformTLSHandshake() start" << std::endl;
+
 		mSSL = SSL_new(mCTX);
+		std::cout << "TlsClientHandler::PerformTLSHandshake() SSL_new(mCTX)" << std::endl;
+
 		SSL_set_fd(mSSL, clientFd);
+		std::cout << "TlsClientHandler::PerformTLSHandshake() SSL_set_fd(mSSL, clientFd)" << std::endl;
 
 		if (SSL_connect(mSSL) <= 0)
 		{
@@ -42,10 +55,14 @@ namespace cipher
 			exit(EXIT_FAILURE);
 		}
 		std::cout << "Connected with " << SSL_get_cipher(mSSL) << " encryption" << std::endl;
+
+		std::cout << "TlsClientHandler::PerformTLSHandshake() end" << std::endl;
 	}
 
 	SSL* TlsClientHandler::GetSSL()
 	{
+		std::cout << "TlsClientHandler::GetSSL() start" << std::endl;
+
 		return mSSL;
 	}
 }
